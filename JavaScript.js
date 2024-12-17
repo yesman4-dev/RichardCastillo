af35491dcf9276829a8f657ece51bd41e7f3b041
yesman4-dev <yesmangunera3@gmail.com>
2024-12-17 10:06:47 -0600

    mejoras en el menu

--- JavaScript.js
+++ JavaScript.js
@@ -64,6 +64,10 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('show');
+}
 
 
 
